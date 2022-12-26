58648129971425a8973a665bfbaa2ac224fa848e
Suel_ki <116934864+Suel-ki@users.noreply.github.com>
2022-12-26 15:23:05 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# FoodEffectTooltips
-Adds Tooltips to food that gives Status Effects when eaten
+# FoodEffectTooltips (Forge)
 
+<img align="right" width="160" src="src/main/resources/icon.png">
+
+[![](http://cf.way2muchnoise.eu/full_701644_downloads.svg)](https://www.curseforge.com/minecraft/mc-mods/food-effect-tooltips-forge)
+[![Minecraft Versions](https://cf.way2muchnoise.eu/versions/701644.svg)](https://www.curseforge.com/minecraft/mc-mods/food-effect-tooltips-forge)
+
+Forge port for [Food Effect Tooltips](https://github.com/DaFuqs/FoodEffectTooltips).
+
+Adds Tooltips to food that gives Status Effects when eaten
 
 Quite a few food items grant the player bonuses in the form of status effects. These effects, however, are never listed ingame. Which is a nuisance, especially in modded scenarios, where the player is not acquainted with all food options and their effects.
 
---
+++ src/main/java/net/suel_ki/foodeffecttooltips/TooltipHelper.java
@@ -0,0 +1,113 @@
+package net.suel_ki.foodeffecttooltips;
+
+import com.google.common.collect.Lists;
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.Registry;
+import net.minecraft.network.chat.CommonComponents;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.StringUtil;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.UseAnim;
+
+import com.mojang.datafixers.util.Pair;
+import net.suel_ki.foodeffecttooltips.config.FoodEffectsConfig;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class TooltipHelper {
+
+    public static boolean shouldShowTooltip(ItemStack stack) {
+        Item item = stack.getItem();
+        ResourceLocation id = Registry.ITEM.getKey(item);
+
+        boolean isWhitelist = FoodEffectsConfig.UseAsWhitelistInstead.get();
+        if(FoodEffectsConfig.BlacklistedItemIdentifiers.get().contains(id.toString())) {
+            return isWhitelist;
+        }
+        if(FoodEffectsConfig.BlacklistedModsIDs.get().contains(id.getNamespace())) {
+            return isWhitelist;
+        }
+        return !isWhitelist;
+    }
+
+    public static void addFoodComponentEffectTooltip(ItemStack stack, List<Component> tooltip) {
+        FoodProperties foodProperties = stack.getItem().getFoodProperties();
+        if(foodProperties != null) {
+            boolean isDrink = stack.getUseAnimation() == UseAnim.DRINK;
+            buildFoodEffectTooltip(tooltip, foodProperties.getEffects(), isDrink);
+        }
+    }
+
+    public static void buildFoodEffectTooltip(List<Component> tooltip, List<Pair<MobEffectInstance, Float>> effectsWithChance, boolean drink) {
+        if(effectsWithChance.isEmpty()) {
+            return;
+        }
+
+        List<Pair<Attribute, AttributeModifier>> modifiersList = Lists.newArrayList();
+        MutableComponent translatableComponent;
+        MobEffect mobEffect;
+        for(Iterator<Pair<MobEffectInstance, Float>> var5 = effectsWithChance.iterator(); var5.hasNext(); tooltip.add(translatableComponent.withStyle(mobEffect.getCategory().getTooltipFormatting()))) {
+            Pair<MobEffectInstance, Float> entry = var5.next();
+            MobEffectInstance mobEffectInstance = entry.getFirst();
+            Float chance = entry.getSecond();
+
+            translatableComponent = Component.translatable(mobEffectInstance.getDescriptionId());
+            mobEffect = mobEffectInstance.getEffect();
+            Map<Attribute, AttributeModifier> map = mobEffect.getAttributeModifiers();
+            if (!map.isEmpty()) {
+                for (Map.Entry<Attribute, AttributeModifier> entityAttributeEntityAttributeModifierEntry : map.entrySet()) {
+                    AttributeModifier entityAttributeModifier = entityAttributeEntityAttributeModifierEntry.getValue();
+                    AttributeModifier entityAttributeModifier2 = new AttributeModifier(entityAttributeModifier.getName(), mobEffect.getAttributeModifierValue(mobEffectInstance.getAmplifier(), entityAttributeModifier), entityAttributeModifier.getOperation());
+                    modifiersList.add(new Pair<>(entityAttributeEntityAttributeModifierEntry.getKey(), entityAttributeModifier2));
+                }
+            }
+
+            if (mobEffectInstance.getAmplifier() > 0) {
+                translatableComponent = Component.translatable("potion.withAmplifier", translatableComponent, Component.translatable("potion.potency." + mobEffectInstance.getAmplifier()));
+            }
+            if (mobEffectInstance.getDuration() > 20) {
+                translatableComponent = Component.translatable("potion.withDuration", translatableComponent, StringUtil.formatTickDuration(mobEffectInstance.getDuration()));
+            }
+            if(chance < 1.0F) {
+                translatableComponent = Component.translatable("foodeffecttooltips.food.withChance", translatableComponent, Math.round(chance * 100));
+            }
+        }
+
+        if (!modifiersList.isEmpty()) {
+            tooltip.add(CommonComponents.EMPTY);
+            if(drink) {
+                tooltip.add(Component.translatable("potion.whenDrank").withStyle(ChatFormatting.DARK_PURPLE));
+            } else {
+                tooltip.add(Component.translatable("foodeffecttooltips.food.whenEaten").withStyle(ChatFormatting.DARK_PURPLE));
+            }
+
+            for (Pair<Attribute, AttributeModifier> entityAttributeEntityAttributeModifierPair : modifiersList) {
+                AttributeModifier entityAttributeModifier3 = entityAttributeEntityAttributeModifierPair.getSecond();
+                double d = entityAttributeModifier3.getAmount();
+                double e;
+                if (entityAttributeModifier3.getOperation() != AttributeModifier.Operation.MULTIPLY_BASE && entityAttributeModifier3.getOperation() != AttributeModifier.Operation.MULTIPLY_TOTAL) {
+                    e = entityAttributeModifier3.getAmount();
+                } else {
+                    e = entityAttributeModifier3.getAmount() * 100.0D;
+                }
+
+                if (d > 0.0D) {
+                    tooltip.add((Component.translatable("attribute.modifier.plus." + entityAttributeModifier3.getOperation().toValue(), ItemStack.ATTRIBUTE_MODIFIER_FORMAT.format(e), Component.translatable((entityAttributeEntityAttributeModifierPair.getFirst()).getDescriptionId()))).withStyle(ChatFormatting.BLUE));
+                } else if (d < 0.0D) {
+                    e *= -1.0D;
+                    tooltip.add((Component.translatable("attribute.modifier.take." + entityAttributeModifier3.getOperation().toValue(), ItemStack.ATTRIBUTE_MODIFIER_FORMAT.format(e), Component.translatable((entityAttributeEntityAttributeModifierPair.getFirst()).getDescriptionId()))).withStyle(ChatFormatting.RED));
+                }
+            }
+        }
+    }
+}
