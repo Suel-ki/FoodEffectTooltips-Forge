54c8da6d9bba5dc3af0b0c666839980f1f566578
Suel_ki <1829446054@qq.com>
2024-11-17 20:22:03 +0800

    Updated to 1.21.3

--- src/main/java/io/github/suel_ki/foodeffecttooltips/TooltipHelper.java
+++ src/main/java/io/github/suel_ki/foodeffecttooltips/TooltipHelper.java
@@ -4,28 +4,27 @@ import com.google.common.collect.Lists;
 import io.github.suel_ki.foodeffecttooltips.config.FoodEffectsConfig;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.Holder;
-import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.StringUtil;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffectUtil;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.ItemUseAnimation;
 
 import com.mojang.datafixers.util.Pair;
+import net.minecraft.world.item.component.Consumable;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
-import net.minecraft.world.item.component.SuspiciousStewEffects;
+import net.minecraft.world.item.consume_effects.ApplyStatusEffectsConsumeEffect;
+import net.minecraft.world.item.consume_effects.ConsumeEffect;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class TooltipHelper {
@@ -44,82 +43,74 @@ public class TooltipHelper {
         return !isWhitelist;
     }
 
-    public static void addFoodComponentEffectTooltip(ItemStack stack, List<Component> tooltip, float tickRate) {
-        FoodProperties foodProperties = stack.get(DataComponents.FOOD);
-        if(foodProperties != null) {
-            boolean isDrink = stack.getUseAnimation() == UseAnim.DRINK;
-            buildFoodEffectTooltip(tooltip, foodProperties.effects(), tickRate, isDrink);
+    public static void addFoodComponentEffectTooltip(@NotNull ItemStack stack, @NotNull Consumable foodProperties, @NotNull List<Component> tooltip, float tickRate) {
+        if(foodProperties.onConsumeEffects().isEmpty()) {
+            return;
         }
+        boolean isDrink = stack.getUseAnimation() == ItemUseAnimation.DRINK;
+        buildFoodEffectTooltip(tooltip, foodProperties.onConsumeEffects(), tickRate, isDrink);
     }
 
-    public static void buildFoodEffectTooltip(List<Component> tooltip, List<FoodProperties.PossibleEffect> effectsWithChance, float tickRate, boolean drink) {
-        if(effectsWithChance.isEmpty()) {
+    public static void buildFoodEffectTooltip(@NotNull List<Component> tooltip, List<ConsumeEffect>  effects, float tickRate, boolean drink) {
+        if(effects.isEmpty()) {
             return;
         }
 
-        List<Pair<Holder<Attribute>, AttributeModifier>> modifiersList = Lists.newArrayList();
-        MutableComponent translatableComponent;
-        MobEffect mobEffect;
-        for(Iterator<FoodProperties.PossibleEffect> var5 = effectsWithChance.iterator(); var5.hasNext(); tooltip.add(translatableComponent.withStyle(mobEffect.getCategory().getTooltipFormatting()))) {
-            FoodProperties.PossibleEffect entry = var5.next();
-            MobEffectInstance mobEffectInstance = entry.effect();
-            float chance = entry.probability();
+        List<Pair<Holder<Attribute>, AttributeModifier>> modifiers = Lists.newArrayList();
+        MutableComponent mutablecomponent;
+        Holder<MobEffect> holder;
 
-            translatableComponent = Component.translatable(mobEffectInstance.getDescriptionId());
-            mobEffect = mobEffectInstance.getEffect().value();
-
-            mobEffect.createModifiers(mobEffectInstance.getAmplifier(), (attributeHolder, attributeModifier) -> {
-                modifiersList.add(new Pair<>(attributeHolder, attributeModifier));
-            });
-
-            if (mobEffectInstance.getAmplifier() > 0) {
-                translatableComponent = Component.translatable("potion.withAmplifier", translatableComponent, Component.translatable("potion.potency." + mobEffectInstance.getAmplifier()));
+        for (ConsumeEffect entry : effects) {
+            if (!(entry instanceof ApplyStatusEffectsConsumeEffect applyEffectsConsumeEffect)) {
+                continue;
             }
-            if (!mobEffectInstance.endsWithin(20)) {
-                translatableComponent = Component.translatable("potion.withDuration", translatableComponent, StringUtil.formatTickDuration(mobEffectInstance.getDuration(), tickRate));
-            }
-            if(chance < 1.0F) {
-                translatableComponent = Component.translatable("foodeffecttooltips.food.withChance", translatableComponent, Math.round(chance * 100));
+            for (MobEffectInstance mobEffectInstance : applyEffectsConsumeEffect.effects()) {
+                mutablecomponent = Component.translatable(mobEffectInstance.getDescriptionId());
+                holder = mobEffectInstance.getEffect();
+                holder.value().createModifiers(mobEffectInstance.getAmplifier(), (attribute, modifier) -> {
+                    modifiers.add(new Pair<>(attribute, modifier));
+                });
+                if (mobEffectInstance.getAmplifier() > 0) {
+                    mutablecomponent = Component.translatable("potion.withAmplifier", mutablecomponent, Component.translatable("potion.potency." + mobEffectInstance.getAmplifier()));
+                }
+
+                if (!mobEffectInstance.endsWithin(20)) {
+                    mutablecomponent = Component.translatable("potion.withDuration", mutablecomponent, MobEffectUtil.formatDuration(mobEffectInstance, 1.0F, tickRate));
+                }
+                if (applyEffectsConsumeEffect.probability() < 1.0F) {
+                    mutablecomponent = Component.translatable("foodeffecttooltips.food.withChance", mutablecomponent, Math.round(applyEffectsConsumeEffect.probability() * 100));
+                }
+
+                tooltip.add(mutablecomponent.withStyle(holder.value().getCategory().getTooltipFormatting()));
             }
         }
 
-        if (!modifiersList.isEmpty()) {
+        if (!modifiers.isEmpty()) {
             tooltip.add(CommonComponents.EMPTY);
-            if(drink) {
+            if (drink) {
                 tooltip.add(Component.translatable("potion.whenDrank").withStyle(ChatFormatting.DARK_PURPLE));
             } else {
                 tooltip.add(Component.translatable("foodeffecttooltips.food.whenEaten").withStyle(ChatFormatting.DARK_PURPLE));
             }
 
-            for (Pair<Holder<Attribute>, AttributeModifier> entityAttributeEntityAttributeModifierPair : modifiersList) {
-                AttributeModifier entityAttributeModifier3 = entityAttributeEntityAttributeModifierPair.getSecond();
-                double d = entityAttributeModifier3.amount();
+            for (Pair<Holder<Attribute>, AttributeModifier> modifier : modifiers) {
+                AttributeModifier attributeModifier = modifier.getSecond();
+                double d = attributeModifier.amount();
                 double e;
-                if (entityAttributeModifier3.operation() != AttributeModifier.Operation.ADD_MULTIPLIED_BASE && entityAttributeModifier3.operation() != AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL) {
-                    e = entityAttributeModifier3.amount();
+                if (attributeModifier.operation() != AttributeModifier.Operation.ADD_MULTIPLIED_BASE && attributeModifier.operation() != AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL) {
+                    e = attributeModifier.amount();
                 } else {
-                    e = entityAttributeModifier3.amount() * 100.0D;
+                    e = attributeModifier.amount() * 100.0;
                 }
 
-                if (d > 0.0D) {
-                    tooltip.add((Component.translatable("attribute.modifier.plus." + entityAttributeModifier3.operation().id(), ItemAttributeModifiers.ATTRIBUTE_MODIFIER_FORMAT.format(e), Component.translatable((entityAttributeEntityAttributeModifierPair.getFirst()).value().getDescriptionId()))).withStyle(ChatFormatting.BLUE));
-                } else if (d < 0.0D) {
-                    e *= -1.0D;
-                    tooltip.add((Component.translatable("attribute.modifier.take." + entityAttributeModifier3.operation().id(), ItemAttributeModifiers.ATTRIBUTE_MODIFIER_FORMAT.format(e), Component.translatable((entityAttributeEntityAttributeModifierPair.getFirst()).value().getDescriptionId()))).withStyle(ChatFormatting.RED));
+                if (d > 0.0) {
+                    tooltip.add(Component.translatable("attribute.modifier.plus." + attributeModifier.operation().id(),  ItemAttributeModifiers.ATTRIBUTE_MODIFIER_FORMAT.format(e), Component.translatable(modifier.getFirst().value().getDescriptionId())).withStyle(ChatFormatting.BLUE));
+                } else if (d < 0.0) {
+                    e *= -1.0;
+                    tooltip.add(Component.translatable("attribute.modifier.take." + attributeModifier.operation().id(),  ItemAttributeModifiers.ATTRIBUTE_MODIFIER_FORMAT.format(e), Component.translatable(modifier.getFirst().value().getDescriptionId())).withStyle(ChatFormatting.RED));
                 }
             }
         }
-    }
-
-    public static List<MobEffectInstance> getStewEffects(ItemStack stew) {
-        List<MobEffectInstance> effects = new ArrayList<>();
-        SuspiciousStewEffects stewEffects = stew.getOrDefault(DataComponents.SUSPICIOUS_STEW_EFFECTS, SuspiciousStewEffects.EMPTY);
-
-        for (SuspiciousStewEffects.Entry effect : stewEffects.effects()) {
-            effects.add(effect.createEffectInstance());
-        }
-
-        return effects;
     }
 
 }
--- src/main/java/io/github/suel_ki/foodeffecttooltips/mixin/ItemStackMixin.java
+++ src/main/java/io/github/suel_ki/foodeffecttooltips/mixin/ItemStackMixin.java
@@ -6,14 +6,14 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.SuspiciousStewItem;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.alchemy.PotionContents;
+import net.minecraft.world.item.component.Consumable;
+import net.minecraft.world.item.component.SuspiciousStewEffects;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,6 +21,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -41,18 +43,23 @@ public class ItemStackMixin {
                     shift = At.Shift.AFTER),
             locals= LocalCapture.CAPTURE_FAILHARD
     )
-    private void getTooltipLines(Item.TooltipContext tooltipContext, Player player, TooltipFlag tooltipFlag, CallbackInfoReturnable<List<Component>> cir, List<Component> tooltips, MutableComponent mutablecomponent, Consumer<Component> consumer) {
+    private void getTooltipLines(Item.TooltipContext tooltipContext, Player player, TooltipFlag tooltipFlag, CallbackInfoReturnable<List<Component>> cir, List<Component> tooltips, Consumer<Component> consumer) {
         ItemStack stack = (ItemStack) (Object) this;
-    if (!stack.isEmpty() && stack.get(DataComponents.FOOD) != null && TooltipHelper.shouldShowTooltip(stack)) {
-
-            if (stack.getItem() instanceof SuspiciousStewItem &&
-                    FoodEffectsConfig.ShowSuspiciousStewTooltips.get() && !tooltipFlag.isCreative()) {
-                List<MobEffectInstance> effects = TooltipHelper.getStewEffects(stack);
-                if (!effects.isEmpty())
-                    PotionContents.addPotionTooltip(effects, tooltips::add, 1.0F, foodeffecttooltips$getTickRate());
+        if (!stack.isEmpty() && TooltipHelper.shouldShowTooltip(stack)) {
+            @Nullable Consumable foodComponent = stack.get(DataComponents.CONSUMABLE);
+            if (foodComponent != null) {
+                TooltipHelper.addFoodComponentEffectTooltip(stack, foodComponent, tooltips, foodeffecttooltips$getTickRate());
             }
-
-            TooltipHelper.addFoodComponentEffectTooltip(stack, tooltips, foodeffecttooltips$getTickRate());
+            if (FoodEffectsConfig.ShowSuspiciousStewTooltips.get() && !tooltipFlag.isCreative()) {
+                @Nullable SuspiciousStewEffects sus = stack.getOrDefault(DataComponents.SUSPICIOUS_STEW_EFFECTS, null);
+                if (sus != null && !sus.effects().isEmpty()) {
+                    List<MobEffectInstance> effects = new ArrayList<>();
+                    for (SuspiciousStewEffects.Entry stewEffect : sus.effects()) {
+                        effects.add(stewEffect.createEffectInstance());
+                    }
+                    PotionContents.addPotionTooltip(effects, tooltips::add, 1.0F, foodeffecttooltips$getTickRate());
+                }
+            }
         }
     }
 
